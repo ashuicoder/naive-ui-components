9e97b59df4af14ba3f088944885e9391bdd5bcac
ashuicoder <1249735120@qq.com>
2024-03-08 09:17:23 +0800

    🐞 fix: modalform

--- app/src/views/Form.vue
+++ app/src/views/Form.vue
@@ -1,70 +1,96 @@
 <template>
   <div>
     <BasicForm @register="register"></BasicForm>
+    <ModalForm v-model:show="showModal" :schemas="schemas" disabled title="2333"></ModalForm>
   </div>
 </template>
 
 <script setup lang="ts">
-import { BasicForm, useForm } from 'naive-ui-form'
+import { BasicForm, useForm, type Recordable, type FormSchema, ModalForm } from 'naive-ui-form'
+import { ref } from 'vue'
+const selectList = ref<Recordable[]>([])
+const schemas: FormSchema[] = [
+  {
+    label: '姓名',
+    field: 'name',
+    type: 'input'
+    // groupName: '分组1'
+  },
+  {
+    label: '年龄',
+    field: 'age',
+    type: 'input-number'
+    // groupName: '分组2'
+  },
+  {
+    label: '信息',
+    field: 'info',
+    type: 'text',
+    defaultValue: '信息展示'
+  },
+  {
+    type: 'radio',
+    field: 'sex',
+    label: '姓别',
+    componentProps: {
+      options: [
+        {
+          label: '男',
+          value: 0
+        },
+        {
+          label: '女',
+          value: 1
+        }
+      ]
+    },
+    defaultValue: 0
+  },
+  {
+    label: '上传',
+    field: 'avatar',
+    type: 'upload',
+    componentProps: {
+      'list-type': 'text'
+    }
+  },
+  {
+    label: '爱好',
+    field: 'hobbies',
+    type: 'select',
+    componentProps: {
+      options: selectList
+    }
+  },
+  {
+    label: '内容',
+    field: 'content',
+    type: 'editor',
+    defaultValue: '信息展示'
+  }
+]
 
+const showModal = ref(true)
 const [register] = useForm({
   grid: {
     cols: 1,
     xGap: 14
   },
-  schemas: [
+  schemas
+})
+
+setTimeout(() => {
+  selectList.value = [
     {
-      label: '姓名',
-      field: 'name',
-      type: 'input'
-      // groupName: '分组1'
+      label: '男',
+      value: 1
     },
     {
-      label: '年龄',
-      field: 'age',
-      type: 'input-number'
-      // groupName: '分组2'
-    },
-    {
-      label: '信息',
-      field: 'info',
-      type: 'text',
-      defaultValue: '信息展示'
-    },
-    {
-      type: 'radio',
-      field: 'sex',
-      label: '姓别',
-      componentProps: {
-        options: [
-          {
-            label: '男',
-            value: 0
-          },
-          {
-            label: '女',
-            value: 1
-          }
-        ]
-      },
-      defaultValue: 0
-    },
-    {
-      label: '上传',
-      field: 'avatar',
-      type: 'upload',
-      componentProps: {
-        'list-type': 'text'
-      }
-    },
-    {
-      label: '内容',
-      field: 'content',
-      type: 'editor',
-      defaultValue: '信息展示'
+      label: '女',
+      value: 2
     }
   ]
-})
+}, 3000)
 </script>
 
 <style scoped></style>
