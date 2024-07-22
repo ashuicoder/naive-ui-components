211b7204e1e4432b7f5446c86aee86edfc5913f3
ashuicoder <1249735120@qq.com>
2024-07-22 16:29:44 +0800

    ✨ feat: ts

--- components/naive-ui-table/src/types/index.ts
+++ components/naive-ui-table/src/types/index.ts
@@ -16,7 +16,7 @@ export type Column<T = Recordable> = TableColumn<T>
 export type Columns<T = Recordable> = TableColumns<T>
 
 export interface TableProps {
-  columns: TableColumns // 列配置项
+  columns: TableColumns<any> // 列配置项
   searchProps?: FormProps // 传给BasicForm的属性
   requestApi?: (params: any) => Promise<any> // 请求表格数据的 api
   requestAuto?: boolean // 是否自动执行请求 api（默认为true）
